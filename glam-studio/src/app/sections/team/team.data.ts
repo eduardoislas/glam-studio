03113f57648cb4eb73c57479d2da5292eee76d70
Eduardo Islas <eduardo@deal-engine.com>
2026-08-24 15:38:23 -0700

    update hero and team sections

--- glam-studio/src/app/sections/team/team.data.ts
+++ glam-studio/src/app/sections/team/team.data.ts
@@ -10,12 +10,12 @@ export const TEAM: TeamMember[] = [
     name: 'Sofía',
     role: 'Maquillaje',
     instagramUrl: 'https://www.instagram.com/sofiaislasmakeup/',
-    photo: '/team/sofia.svg',
+    photo: '/team/sofia.png',
   },
   {
     name: 'Perla Huerta',
     role: 'Peinado',
     instagramUrl: 'https://www.instagram.com/perlahuerta.hair/',
-    photo: '/team/perla.svg',
+    photo: '/team/perla.png',
   },
 ];
